6532e7ad33d9766d0f87557d9d32f529b31aab0f
karenhernandez <khernandez0329@gmail.com>
2023-10-26 18:46:09 -0700

    add Return to Top button

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+        }
+    function topFunction() {
+        document.documentElement.scrollTop = 0;
+    }
